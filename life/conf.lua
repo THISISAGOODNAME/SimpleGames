09b85789aa0f7dcd734ed40422d01f10fcdf17fd
THISISAGOODNAME <yangyj19931231@gmail.com>
2023-03-20 23:41:58 +0800

    Life

---
+++ life/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.title = "Life [Left click]	Make cell alive [Right click]	Make cell dead [Any key] Step forward in time"
+    t.window.width = 5 * 70
+    t.window.height = 5 * 50
+end
---
+++ life/main.lua
@@ -0,0 +1,79 @@
+function love.load()
+    -- For ZeroBrane debugger
+    -- if arg[#arg] == "-debug" then require("mobdebug").start() end
+    
+    love.graphics.setBackgroundColor(1, 1, 1)
+    
+    cellSize = 5
+    
+    gridXCount = 70
+    gridYCount = 50
+    
+    grid = {}
+    for y = 1, gridYCount do
+        grid[y] = {}
+        for x = 1, gridXCount do
+            grid[y][x] = false
+        end
+    end
+    
+    love.keyboard.setKeyRepeat(true)
+end
+
+function love.update(dt)
+    selectedX = math.min(math.floor(love.mouse.getX() / cellSize) + 1, gridXCount)
+    selectedY = math.min(math.floor(love.mouse.getY() / cellSize) + 1, gridYCount)
+    
+    if love.mouse.isDown(1) then
+        grid[selectedY][selectedX] = true
+    elseif love.mouse.isDown(2) then
+        grid[selectedY][selectedX] = false
+    end
+end
+
+function love.keypressed(key)
+    local nextGrid = {}
+
+    for y = 1, gridYCount do
+        nextGrid[y] = {}
+        for x = 1, gridXCount do
+            local neighborCount = 0
+
+            for dy = -1, 1 do
+                for dx = -1, 1 do
+                    if not (dy == 0 and dx == 0)
+                    and grid[y + dy]
+                    and grid[y + dy][x + dx] then
+                        neighborCount = neighborCount + 1
+                    end
+                end
+            end
+
+            nextGrid[y][x] = neighborCount == 3 or (grid[y][x] and neighborCount == 2)
+        end
+    end
+
+    grid = nextGrid
+end
+
+function love.draw()
+    for y = 1, gridYCount do
+        for x = 1, gridXCount do
+            local cellDrawSize = cellSize - 1
+            
+            if x == selectedX and y == selectedY then
+                love.graphics.setColor(0, 1, 1)
+            elseif grid[y][x] then
+                love.graphics.setColor(1, 0, 1)
+            else
+                love.graphics.setColor(.86, .86, .86)
+            end
+            
+            love.graphics.rectangle("fill", (x - 1) * cellSize, (y - 1) * cellSize, cellDrawSize, cellDrawSize)
+            
+            -- love.graphics.setColor(0, 0, 0)
+            -- love.graphics.print('selected x: '..selectedX..', selected y: '..selectedY)
+        end
+    end
+
+end
